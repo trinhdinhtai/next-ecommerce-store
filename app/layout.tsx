76e2ca850c89a6771bde4951a7474cbfd3711787
trinhdinhtai <142890841+trinhdinhtai@users.noreply.github.com>
2023-08-28 16:48:46 +0700

    chore: refactor title property in metadata object to support dynamic site name and wrap main content in Suspense component for better error handling and lazy loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,11 +6,17 @@ import Navbar from "@/components/layout/navbar";
 import Footer from "@/components/layout/footer";
 import ModalProvider from "@/providers/modal-provider";
 import { ThemeProvider } from "@/providers/theme-provider";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const { SITE_NAME } = process.env;
+
 export const metadata: Metadata = {
-  title: "Anon",
+  title: {
+    default: SITE_NAME!,
+    template: `%s | ${SITE_NAME}`,
+  },
   description: "A simple e-commerce site built with Next.js and Tailwind CSS.",
 };
 
@@ -26,7 +32,9 @@ export default function RootLayout({
           <ToastProvider />
           <ModalProvider />
           <Navbar />
-          {children}
+          <Suspense>
+            <main>{children}</main>
+          </Suspense>
           <Footer />
         </ThemeProvider>
       </body>
