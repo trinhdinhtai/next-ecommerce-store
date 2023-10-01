c5c6efb76c4f4623cc19074621fd1424e3701c3b
Nguyễn Hoàn <hoandev0308@gmail.com>
2023-10-01 13:20:57 +0700

    Update product

--- components/category/categories-carousel.tsx
+++ components/category/categories-carousel.tsx
@@ -14,16 +14,17 @@ interface CarouselProps {
 }
 
 const CategoriesCarousel = ({ categories }: CarouselProps) => {
+  const lengthCategories = categories.length
   const carouselSettings = {
     dots: false,
     infinite: true,
     draggable: false,
     speed: 500,
-    slidesToShow: 10,
-    slidesToScroll: 10,
-    rows: 2,
+    slidesToShow: lengthCategories < 10 ? 3 : 10,
+    slidesToScroll: lengthCategories < 10 ? 3 : 10,
+    rows: lengthCategories < 10 ? 1 : 2,
     autoplaySpeed: 8000,
-    autoplay: true,
+    autoplay: false,
     prevArrow: <PrevIcon />,
     nextArrow: <NextIcon />,
   }
@@ -31,19 +32,19 @@ const CategoriesCarousel = ({ categories }: CarouselProps) => {
   return (
     <div className="">
       <Slider {...carouselSettings}>
-        {categories?.map((category, i) => (
+        {categories?.map((category) => (
           <Link
             href={`categories/${category.id}`}
             key={category.id}
             className="h-[160px] w-[120px] border p-2"
           >
-            <div className="flex flex-col items-center justify-center">
-              <div className="h-[70px] w-[70px] gap-4 rounded-full">
+            <div className="mt-2 flex flex-col items-center justify-center">
+              <div className="relative h-[70px] w-[70px] gap-4 rounded-md">
                 <Image
-                  src={`/categories/${i}.png`}
+                  src={category.imageUrl}
                   alt="category"
-                  width={120}
-                  height={150}
+                  className="rounded-md border object-cover"
+                  fill
                 />
               </div>
               <div className="font-sm m-2 w-[98%] text-center text-sm font-normal">
--- types/index.ts
+++ types/index.ts
@@ -1,39 +1,40 @@
 export interface Product {
-  id: string;
-  category: Category;
-  name: string;
-  price: string;
-  isFeatured: boolean;
-  size: Size;
-  color: Color;
-  images: Image[];
+  id: string
+  category: Category
+  name: string
+  price: string
+  isFeatured: boolean
+  size: Size
+  color: Color
+  images: Image[]
 }
 
 export interface Image {
-  id: string;
-  url: string;
+  id: string
+  url: string
 }
 
 export interface Billboard {
-  id: string;
-  label: string;
-  imageUrl: string;
+  id: string
+  label: string
+  imageUrl: string
 }
 
 export interface Category {
-  id: string;
-  name: string;
-  billboard: Billboard;
+  id: string
+  name: string
+  billboard: Billboard
+  imageUrl: string
 }
 
 export interface Size {
-  id: string;
-  name: string;
-  value: string;
+  id: string
+  name: string
+  value: string
 }
 
 export interface Color {
-  id: string;
-  name: string;
-  value: string;
+  id: string
+  name: string
+  value: string
 }
