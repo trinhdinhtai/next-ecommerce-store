196b22b755179f50b134508bff8c68a127dc98f9
taitd-gms <142890841+trinhdinhtai@users.noreply.github.com>
2023-10-03 18:32:37 +0700

    chore: import environment variables from separate module for better organization and maintainability

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { Suspense } from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 
+import { env } from "@/env.mjs"
+import { cn } from "@/lib/utils"
 import Footer from "@/components/layout/footer"
 import Navbar from "@/components/layout/navbar"
 import ModalProvider from "@/components/providers/modal-provider"
@@ -11,14 +13,17 @@ import { ThemeProvider } from "@/components/providers/theme-provider"
 import ToasterProvider from "@/components/providers/toaster-provider"
 import ScrollToTopButton from "@/components/scroll-to-top-button"
 
-const inter = Inter({ subsets: ["latin"] })
-
-const { SITE_NAME } = process.env
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+})
 
 export const metadata: Metadata = {
+  metadataBase: new URL(env.NEXT_PUBLIC_APP_URL),
   title: {
-    default: SITE_NAME!,
-    template: `%s | ${SITE_NAME}`,
+    default: env.SITE_NAME,
+    template: `%s - ${env.SITE_NAME}`,
   },
   description: "A simple e-commerce site built with Next.js and Tailwind CSS.",
 }
@@ -30,7 +35,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={cn(inter.variable, "font-sans")}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <ToasterProvider />
           <ModalProvider />
