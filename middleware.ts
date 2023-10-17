2785257c7ff944415411917085e1b1359ac856ed
taitd-gms <142890841+trinhdinhtai@users.noreply.github.com>
2023-10-17 13:08:47 +0700

    feat: update authMiddleware to redirect unauthenticated users to sign-in page

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
-import { authMiddleware } from "@clerk/nextjs"
+import { NextResponse } from "next/server"
+import { authMiddleware, clerkClient } from "@clerk/nextjs"
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
@@ -11,7 +12,31 @@ export default authMiddleware({
     "/sign-up(.*)",
     "/sign-out(.*)",
     "/sso-callback(.*)",
+    "/categories(.*)",
+    "/api(.*)",
   ],
+  async afterAuth(auth, req) {
+    if (auth.isPublicRoute) {
+      //  For public routes, we don't need to do anything
+      return NextResponse.next()
+    }
+
+    const url = new URL(req.nextUrl.origin)
+
+    if (!auth.userId) {
+      //  If user tries to access a private route without being authenticated,
+      //  redirect them to the sign in page
+      url.pathname = "/sign-in"
+      return NextResponse.redirect(url)
+    }
+
+    // Set the user's role to user if it doesn't exist
+    const user = await clerkClient.users.getUser(auth.userId)
+
+    if (!user) {
+      throw new Error("User not found.")
+    }
+  },
 })
 
 export const config = {
